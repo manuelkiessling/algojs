10a4366343c8dd0bfc2cb97469d10d68d9d4bc6b
Manuel Kiessling <manuel@kiessling.net>
2012-04-02 23:01:26 +0200

    an ok looking graph

--- app/index.js
+++ app/index.js
@@ -5,15 +5,15 @@ define(['../lib/EdgeNode', '../lib/insertEdge', '../lib/drawer/canvas'],
       edges: []
     };
 
-    insertEdge(graph, 1, 66, EdgeNode);
+    insertEdge(graph, 32, 66, EdgeNode);
     insertEdge(graph, 72, 66, EdgeNode);
-    insertEdge(graph, 66, 15, EdgeNode);
-    insertEdge(graph, 15, 1, EdgeNode);
+    insertEdge(graph, 66, 14, EdgeNode);
+    insertEdge(graph, 14, 32, EdgeNode);
     insertEdge(graph, 69, 66, EdgeNode);
     insertEdge(graph, 88, 69, EdgeNode);
     insertEdge(graph, 100, 88, EdgeNode);
     insertEdge(graph, 28, 69, EdgeNode);
-    insertEdge(graph, 6, 15, EdgeNode);
+    insertEdge(graph, 26, 14, EdgeNode);
 
     canvasDrawer.init(document.getElementById('canvas'), 10, 10, graph);
 
