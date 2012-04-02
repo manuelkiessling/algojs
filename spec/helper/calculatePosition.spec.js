8f31bfa6e82056dffab6a16697f67b72dd898789
Manuel Kiessling <manuel@kiessling.net>
2012-04-02 17:36:36 +0200

    added helper function that calulates graph element position in 2d space

---
+++ spec/helper/calculatePosition.spec.js
@@ -0,0 +1,41 @@
+"use strict";
+
+var calculatePosition = require("../../lib/helper/calculatePosition");
+
+describe("calculatePosition", function() {
+  it("places item 8 at 448,0", function() {
+    var options = {
+      width: 640,
+      height: 500,
+      rows: 10,
+      columns: 10
+    };
+    var position = calculatePosition(8, options);
+    expect(position.x).toEqual(448);
+    expect(position.y).toEqual(0);
+  });
+
+  it("places item 10 at 576,0", function() {
+    var options = {
+      width: 640,
+      height: 500,
+      rows: 10,
+      columns: 10
+    };
+    var position = calculatePosition(10, options);
+    expect(position.x).toEqual(576);
+    expect(position.y).toEqual(0);
+  });
+
+  it("places item 11 at 0,50", function() {
+    var options = {
+      width: 640,
+      height: 500,
+      rows: 10,
+      columns: 10
+    };
+    var position = calculatePosition(11, options);
+    expect(position.x).toEqual(0);
+    expect(position.y).toEqual(50);
+  });
+});
