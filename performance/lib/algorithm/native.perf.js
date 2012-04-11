c91c0da26fe335b0a00e562e486d86fc62e78eed
Manuel Kiessling <manuel@kiessling.net>
2012-04-11 20:56:18 +0200

    added native JS Array.sort() benchmark

---
+++ performance/lib/algorithm/native.perf.js
@@ -0,0 +1,13 @@
+var numItems = 10000000;
+var items = [];
+for (var i = 0; i < numItems; i++) {
+  items.push(Math.floor(Math.random() * numItems));
+}
+
+var start = new Date().getTime();
+items.sort();
+var end = new Date().getTime();
+var time = end - start;
+console.log(numItems + " items sorted");
+console.log(time / 1000 + " seconds");
+console.log((numItems / time) * 1000 + " items / second");
