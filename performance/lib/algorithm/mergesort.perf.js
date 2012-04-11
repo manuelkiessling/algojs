321d0c407d919564a3669d98f69dd9aa47464e68
Manuel Kiessling <manuel@kiessling.net>
2012-04-11 20:56:01 +0200

    better result output

--- performance/lib/algorithm/mergesort.perf.js
+++ performance/lib/algorithm/mergesort.perf.js
@@ -26,14 +26,17 @@ define(["../../../lib/datastructure/Queue", "../../../lib/algorithm/mergesort"],
     }
   };
 
+  var numItems = 10000000;
   var items = new Queue();
-  for (var i = 0; i < 10000000; i++) {
-    items.enqueue(Math.floor(Math.random() * 10000000));
+  for (var i = 0; i < numItems; i++) {
+    items.enqueue(Math.floor(Math.random() * numItems));
   }
 
   var start = new Date().getTime();
   var sorted = mergesort(items, compareNumbers);
   var end = new Date().getTime();
   var time = end - start;
-  console.log(time);
+  console.log(numItems + " items sorted");
+  console.log(time / 1000 + " seconds");
+  console.log((numItems / time) * 1000 + " items / second");
 });
--- performance/lib/algorithm/mergesortArray.perf.js
+++ performance/lib/algorithm/mergesortArray.perf.js
@@ -13,7 +13,7 @@ var compareNumbers = function(a, b) {
   }
 };
 
-define(["../../../lib/datastructure/Queue", "../../../lib/algorithm/mergesortArray"], function(Queue, mergesort) {
+define(["../../../lib/algorithm/mergesortArray"], function(mergesort) {
   var compareNumbers = function(a, b) {
     if (a < b) {
       return -1;
@@ -26,14 +26,17 @@ define(["../../../lib/datastructure/Queue", "../../../lib/algorithm/mergesortArr
     }
   };
 
+  var numItems = 10000000;
   var items = [];
-  for (var i = 0; i < 10000000; i++) {
-    items.push(Math.floor(Math.random() * 10000000));
+  for (var i = 0; i < numItems; i++) {
+    items.push(Math.floor(Math.random() * numItems));
   }
 
   var start = new Date().getTime();
   var sorted = mergesort(items, compareNumbers);
   var end = new Date().getTime();
   var time = end - start;
-  console.log(time);
+  console.log(numItems + " items sorted");
+  console.log(time / 1000 + " seconds");
+  console.log((numItems / time) * 1000 + " items / second");
 });
